de0b163b68875fb284d1b9f7061e5e50e238797f
Miltt4869 <milttse184379@fpt.edu.vn>
2025-05-26 19:43:07 +0700

    Add files via upload

---
+++ Code/Chapter3/OOP1/Employee.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class Employee
+    {
+        private int _id;    //instance variable
+
+        private string _name;//instance variable
+
+        private string _email;
+        private string _phone;
+
+        // ưu tiên xử lý cho local nếu cả 2 có cùng tên
+
+        #region Nhóm các contructer của empl
+        public Employee() 
+        { 
+
+        }
+       public Employee(int _id,  string _name, string _email, string _phone) //local variable
+        {
+            this._id = _id;
+            this._name = _name;
+            //or
+            Email = _email; // gọi là setter cho proterty Email 
+            Phone = _phone;
+
+        }
+
+        #endregion
+        #region Nhóm các set/get
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+       
+        public string Name {
+            get { return _name; }
+            set { _name = value;}
+        }
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        public string Phone
+        {
+            get { return _phone; }
+            set { _phone = value; }
+        }
+        #endregion
+        public void PrintInfer()
+        {
+            Console.WriteLine($"{_id}\t {_name}\t {_email}\t {_phone}");
+        }
+
+        public override string ToString()
+        {
+            string msg = $"{_id}\t {_name}\t {_email}\t {_phone}";
+            return msg;
+
+        }
+
+
+    }
+}
---
+++ Code/Chapter3/OOP1/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using OOP1;
+
+Category c1 = new Category();
+c1.Id = 1;
+c1.Name = "Nước nắm";
+Console.OutputEncoding = Encoding.UTF8;
+c1.PrintInfer();
+
+Employee employee = new Employee();
+/*employee._id = 1;
+ * 
+employee._name = "tèo "; */
+// đề thay đoỏi giá trị của thuộc tính
+employee.Id = 1;
+employee.Name = "Tèo";
+employee.Phone = "225";
+employee.Email = "123@gmail.com";
+// tự lấy gti thuộc tính
+// tuẹ gọi prototype id
+Console.WriteLine($"Employee ID = {employee.Id}");
+// tự gọi ---- Name
+Console.WriteLine($"Employee Name = {employee.Name}");
+Console.WriteLine($"Employee Email = {employee.Email}");
+Console.WriteLine($"Employee Phone = {employee.Phone}");
+employee.PrintInfer();
+
+// ngoài ra mọi lớp đối tượng có hàm toString() giống java
+// để tuẹ động triệu gọi hàm 
+
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine(employee);
+
+//vừa tạo đối tượng khởi tạo giá trị cho thuộc tính:
+Employee employee2 = new Employee(2, "Tý Đại Bàng", "ty@yahoo.com", "0987654321");
+//xuất thông tin của emp2
+Console.WriteLine(employee2);
+//hoặc ta có thể coding như sau:
+Employee emp3 = new Employee()
+{
+    Id = 3,
+    Name = "Tủn",
+    Email = "tun@hotmail.com",
+    Phone = "0897654321"
+};
+//xuất thông tin của emp3
+Console.WriteLine(emp3);
+
