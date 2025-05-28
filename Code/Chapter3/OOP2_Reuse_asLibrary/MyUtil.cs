502d362103db8d7dc555f791d131edc412321403
Miltt4869 <milttse184379@fpt.edu.vn>
2025-05-28 13:26:20 +0700

    Add files via upload

---
+++ Code/Chapter3/OOP2_Reuse_asLibrary/MyUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOP2;
+
+namespace OOP2_Reuse_asLibrary
+{
+    public static class MyUtil
+    {
+        public static int TinhTuoi(this Employess emp)
+        {
+            return DateTime.Now.Year - emp.Birthday.Year + 1;
+        }
+    }
+}
---
+++ Code/Chapter3/OOP2_Reuse_asLibrary/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using OOP2;
+using OOP2_Reuse_asLibrary;
+FulltimeEmployess e1 = new FulltimeEmployess()
+
+{
+    ID = 1,
+    Name = "Tèo",
+    Birthday = new DateTime(1960, 1, 1)
+};
+Console.WriteLine("----Thông tin E1----");
+Console.WriteLine(e1);
+Console.WriteLine("AGE===> " + e1.TinhTuoi());
+
