2cbe8b1c100adb1a59c39534f03e85244d60955a
Miltt4869 <milttse184379@fpt.edu.vn>
2025-05-28 13:27:40 +0700

    Add files via upload

---
+++ Code/Chapter3/OOP3/MyUtils.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    public static class MyUtils
+    {
+        // cài đặt hàm tính tổng từ 1-n
+        // vào kiểu int Microsoft
+        public static int Tongtu1toiN (this int n)
+        {
+            int sum = 0;
+            for (int i = 0; i<= n; i++)
+            {
+                sum += 1;
+            }
+            return sum;
+        }
+
+        public static void SapxepTangdan(this int[] ar)
+        {
+            for (int i = 0; i <= ar.Length; i++)
+            {
+                for (int j = i; j < ar.Length; j++)
+                {
+                    if (ar[i] >= ar[j])
+                    {
+                        int temp = ar[i];
+                        ar[i] = ar[j];
+                        ar[j] = temp;
+                    }
+                    
+                }
+            }
+        }
+        public static void TaoMangNgauNhien(this int[] ar)
+        {
+            Random rd = new Random();
+            for (int i = 0; i <= ar.Length; i++)
+            {
+                ar[i] = rd.Next(100);
+            }
+        }
+        public static void XuatMang(this int[] ar)
+        {
+            foreach (int i in ar)
+            {
+                Console.WriteLine(i + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Code/Chapter3/OOP3/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using OOP3;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+int n1 = 5;
+int n2 = 10;
+Console.WriteLine("Tổng n1 = " + n1.Tongtu1toiN());
+Console.WriteLine("Tổng n2 = " + n2.Tongtu1toiN());
+
+int[] M = new int[10];
+M.TaoMangNgauNhien();
+Console.WriteLine("Mảng trước khi sắp xếp: ");
+M.XuatMang();
+M.SapxepTangdan();
+Console.WriteLine("Mảng sau khi sắp xếp:");
+M.XuatMang();
