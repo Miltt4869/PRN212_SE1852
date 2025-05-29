7d6c7d0c32f673b6daa4b4955de2efc17e0c6be5
Miltt4869 <milttse184379@fpt.edu.vn>
2025-05-29 07:12:48 +0700

    Add files via upload

---
+++ Code/chapter02/SimpleMath/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+string do_math(double a, double b, string op)
+{
+    string result = "";
+    switch (op)
+    {
+        case "+":
+            result = a +"+"+ b + "=" + (a+b); 
+            break;
+        case "-":
+            result = a + "-" + b + "=" + (a - b);
+            break;
+        case "*":
+            result = a + "*" + b + "=" + (a * b);
+            break;
+        case "/":
+            if (b == 0)
+            {
+                result = "Mẫu số không hợp lệ";
+            }
+            else {
+                result = a + "/" + b + "=" + (a / b);
+            }
+            break;  
+        default:
+            result = "Xin vui lòng nhập đúng";
+            break;
+
+    }
+    return result;
+}
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Nhap a: ");
+double a =Double.Parse(Console.ReadLine());
+Console.WriteLine("Nhap b: ");
+double b =Double.Parse(Console.ReadLine());
+Console.WriteLine("Nhap pheps toans(+,-,*,/): ");
+string op = Console.ReadLine();
+string result = do_math(a, b, op);
+Console.WriteLine(result);
+
+Console.ReadLine();
---
+++ Code/chapter02/StringInterraction/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Hello, World!");
+string ho = "Nguyễn";
+string tenlot = "Thị";
+string ten = "Tèo";
+string tenfull = ho + " "+tenlot + " "+ten;
+Console.WriteLine(tenfull);
+StringBuilder sb = new StringBuilder();  // cọng chỗi sẽ chậm, nên dùng string builder sẽ nhanh hơn
+sb.Append(ho);
+sb.Append(" ");
+sb.Append(tenlot);
+sb.Append(" ");
+sb.Append(ten);
